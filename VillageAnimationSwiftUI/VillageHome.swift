0923a745b60cbc045ecadf2595bf1e3cb27053d6
MaratXcode <kosjachok8@yandex.ru>
2024-08-15 00:34:45 +0300

    work with view

---
+++ VillageAnimationSwiftUI/VillageHome.swift
@@ -0,0 +1,18 @@
+//
+//  VillageHome.swift
+//  VillageAnimationSwiftUI
+//
+//  Created by Marat Fakhrizhanov on 14.08.2024.
+//
+
+import SwiftUI
+
+struct VillageHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VillageHome()
+}
---
+++ VillageAnimationSwiftUI/WindowHome.swift
@@ -0,0 +1,18 @@
+//
+//  WindowHome.swift
+//  VillageAnimationSwiftUI
+//
+//  Created by Marat Fakhrizhanov on 14.08.2024.
+//
+
+import SwiftUI
+
+struct WindowHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WindowHome()
+}

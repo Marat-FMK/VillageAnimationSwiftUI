00e7b7af8660dbb275ce5aaa7cda51e72dfa7e0b
MaratXcode <kosjachok8@yandex.ru>
2024-08-14 21:59:52 +0300

    Initial Commit

---
+++ VillageAnimationSwiftUI/VillageAnimationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VillageAnimationSwiftUIApp.swift
+//  VillageAnimationSwiftUI
+//
+//  Created by Marat Fakhrizhanov on 14.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct VillageAnimationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
